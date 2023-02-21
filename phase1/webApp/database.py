ab6da94184138b824d32297c677142e9698421e7
maeng <ggmaeng@gmail.com>
2023-02-21 09:19:33 +0900

    #35 turbo-flask 실험 실패

--- phase1/webApp/database.py
+++ phase1/webApp/database.py
@@ -4,7 +4,7 @@ from firebase_admin import firestore
 from datetime import datetime
 
 # Use a service account.
-cred = credentials.Certificate('../../../secret/firebase_key/nugunaaiot-maeng-1004a11a5af7.json') #aiot-nuguna-03687aeaa9e6.json
+cred = credentials.Certificate('../../secret/nugunaaiot-maeng-1004a11a5af7.json')
 
 app = firebase_admin.initialize_app(cred)
 
@@ -15,24 +15,4 @@ def get_device(collection, d_id):
     device = db.collection(collection).document(d_id).get()
     if device.exists:
         return device.to_dict()
-
-# def create_device(d_id):
-#     doc_ref = db.collection('device').document(d_id)
-#     if not doc_ref.get().exists:
-#         device = {d_id: {
-#             'id': d_id,
-#             'is_running': False,
-#             'manufacture_date': '20221213',
-#             'sensor': []
-#         }}
-#         doc_ref.set(device)
-#
-#
-# def upload_sensor(d_id, sensor):
-#     doc_ref = db.collection('device').document(d_id)
-#     sensor['update_time'] = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
-#     doc_ref.update({'sensor': firestore.ArrayUnion([sensor])})
-#
-
-
 
