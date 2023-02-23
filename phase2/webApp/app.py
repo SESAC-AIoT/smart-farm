7edf6b55fe122281826e25fffcb649b5e631c3cc
maeng <ggmaeng@gmail.com>
2023-02-23 15:26:09 +0900

    phase2 integrate finish

--- phase2/webApp/app.py
+++ phase2/webApp/app.py
@@ -36,7 +36,6 @@ def shutdown_session(exception=None):
 def monitor():
     # 화면 리프레시 할 때 데이터 새로 고침을 위해 세션 미사용
     device = get_device(collection, d_id)
-    print(device['sensor'][-1]) # 가장 마지막(최근) 데이터
     m = get_monitor(device['sensor'][-1])
     g = get_growth((device['model'][-1]))
     return render_template('monitor.html', m = m, growth = g)
@@ -47,7 +46,6 @@ def board(type):
     device = get_device(collection, d_id)
     sensors, times = get_board(device)
     values = get_chart(device, type)
-    print(values)
     return render_template('board.html', sensors = sensors , times = times, values = values)
 
 def get_growth(growth):
@@ -144,16 +142,16 @@ def detect_table():
     all_list = device['detect'][-20:]
     all_list.reverse()
     results = [all for all in all_list]
-    return render_template('detect_table.html', times = results)
+    return render_template('detect_table.html', data = results)
 
 # 웹캠 실행
 @app.route('/webcam')
 def webcam():
-    return Response(gen_frame(),
+    return Response(webcam_gen_frame(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # 웹캠 opencv 실행 및 yolo 탐지
-def gen_frame():
+def webcam_gen_frame():
     cam = cv2.VideoCapture(0, cv2.CAP_DSHOW) # 웹캠 opencv 로딩 속도 개선 cap_dshow 다이렉트쇼
     while(cam.isOpened()):
         success, frame = cam.read()
@@ -163,7 +161,7 @@ def gen_frame():
             img = Image.open(io.BytesIO(frame))
 
             results = model(img, size=640)
-            upload_detect(results)
+            upload_detect(str(results), input='webcam')
 
             img = np.squeeze(results.render()) #RGB
             img_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) #BGR
@@ -174,30 +172,28 @@ def gen_frame():
         yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 # 객체탐지 DB 생성 및 업로드
-def upload_detect(results):
-    results_raw = results
-    results = str(results)
-    if 'cat' in results:
-        # print(str(results).split())
-        # print(str(results)[19:26])
-        detect_log = results[19:26]
+def upload_detect(preds, input, filename=None):
+    obj = 'cat'
+    if obj in preds:
+        if input == 'webcam':
+            filename = 'realtime.jpg'
+            predict_log = preds[19:26]
+        elif input == 'userfile':
+            predict_log = 99  # 분류의 경우 positive
+
         detect_data = {'update_time': datetime.now().strftime("%Y.%m.%d %H:%M:%S"),
-                       'class' : '고양이',
-                       'count' : detect_log[0],# [0:1]
-                       'input' : '웹캠',
-                       'filename' : datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + "_webcam_image.jpg"
+                       'class': obj,
+                       'count': predict_log,
+                       'input': input,
+                       'filename': filename
                        }
+
         doc_ref = db.collection(collection).document(d_id)
         doc_ref.update({'detect': firestore.ArrayUnion([detect_data])})
 
-        # img = np.squeeze(results_raw.render())  # RGB
-        # img_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)  # BGR
-        #
-        # filename= str(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")) + "_webcam_image.jpg"
-        # img_path = "./static/" +filename
-        # cv2.imwrite(img_path, img_BGR)
-
-
+def file_save(img, filename): # 탐지완료 파일 백업용
+    img_path = "../../secret/output/" + filename
+    cv2.imwrite(img_path, img)
 
 ############### 사용자파일 객체탐지관련 웹페이지 및 함수 #################
 
@@ -211,147 +207,26 @@ def detect_upload():
 def file_upload():
     if request.method == 'POST':
         if 'file' not in request.files:
-            # flash('No file part')
             return 'You forgot Snap!'
         fs = request.files.get('file')
-        # print(fs.read(), file=sys.stderr)
-        if 'jpg' in fs.filename:
+        filename = fs.filename
+
+        if 'jpg' in filename:
             img = cv2.imdecode(np.frombuffer(fs.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-            pred = predict_image(img)
+            file_save(img, filename)
+
+            pred = predict_image(img) # 분류 예측
+            upload_detect(pred, input='userfile', filename=filename)
+            
+            # 웹에서 디스플레이 되도록 인코딩
             ret, buf = cv2.imencode('.jpeg', img)
             b64_img = base64.b64encode(buf).decode('utf-8')
-
-            # 데이터베이스 추가 (고양이 예측시)
-            if pred == "cat":
-                detect_data = {'update_time': 'yyyy.mm.dd hh:mm:ss'}
-                detect_data['update_time'] = datetime.now().strftime("%Y.%m.%d %H:%M:%S")
-                detect_data['class'] = "고양이"
-                detect_data['count'] = 1
-                detect_data['input'] = "사용자파일_이미지"
-                detect_data["filename"] = str(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")) + "_userfile_image.jpg"
-                doc_ref = db.collection(collection).document(d_id)
-                doc_ref.update({'detect': firestore.ArrayUnion([detect_data])})
-
-                img_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)  # BGR
-                filename = str(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")) + "_userfile_image.jpg"
-                img_path = "./static/" + filename
-                cv2.imwrite(img_path, img_BGR)
-
-            return render_template('detect_upload.html', image=b64_img, predict=pred, filename=None)
-
-        # elif 'mp4' in fs.filename:
-        #     with tempfile.TemporaryDirectory() as td:
-        #         temp_filename = Path(td) / 'uploaded_video'
-        #         fs.save(temp_filename)
-        #         vidcap = cv2.VideoCapture(str(temp_filename))
-        #         filename = batch_frames(vidcap)
-        #
-        #         # 데이터베이스 추가
-        #         if pred == "cat":
-        #             detect_data = {'update_time': 'yyyy.mm.dd hh:mm:ss'}
-        #             detect_data['update_time'] = datetime.datetime.now().strftime("%Y.%m.%d %H:%M:%S")
-        #             detect_data['class'] = "고양이"
-        #             detect_data['count'] = 1
-        #             detect_data['input'] = "사용자파일_영상"
-        #             detect_data["filename"] = str(
-        #                 datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")) + "_userfile_mp4.jpg"
-        #             doc_ref = db.collection(collection).document(d_id)
-        #             doc_ref.update({'detect': firestore.ArrayUnion([detect_data])})
-        #
-        #             img_BGR = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)  # BGR
-        #             filename = str(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")) + "_userfile_mp4.jpg"
-        #             img_path = "./static/" + filename
-        #             cv2.imwrite(img_path, img_BGR)
-        #
-        #         return send_file('./static/{}'.format(filename), download_name=filename, as_attachment=True)
+            return render_template('detect_upload.html', image = b64_img ,predict=pred, filename=None)
 
         else:
             return 'You forgot Snap!'
 
     return '빈페이지'
-
-
-
-# 파일 교환 관련 함수(송신)
-def send_file_data(data, mimetype='image/jpeg', filename='output.jpg'):
-    response = make_response(data)
-    response.headers.set('Content-Type', mimetype)
-    response.headers.set('Content-Disposition', 'attachment', filename=filename)
-    return response
-
-# 파일 교환 관련 함수(객체탐지_단일이미지)
-def gen_frames(cap):
-    w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    filename = 'pred_video.mp4'
-    out = cv2.VideoWriter('./static/' + filename, fourcc, 30, (w, h))
-    cnt = 0
-    while True:
-        success, frame = cap.read()  # read the camera frame
-        if not success:
-            break
-        cnt += 1
-        if cnt % 3 == 0:
-            continue
-        pred = predict_image(frame)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        org = (300, 100)
-        fontScale = 1
-        color = (255, 0, 0)
-        thickness = 2
-        frame = cv2.putText(frame, pred, org, font,
-                            fontScale, color, thickness, cv2.LINE_AA)
-        out.write(frame)
-    cap.release()
-    out.release()
-    return filename
-
-
-# 파일 교환 관련 함수(객체탐지_영상이미지)
-def batch_frames(cap):
-    # 여러프레임 한번에 예측 (영상화 시킬때 사용)
-    w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    filename = 'pred_video.mp4'
-    out = cv2.VideoWriter('./static/'+filename, fourcc, 30, (w, h))
-    cnt = 0
-    origins = []
-    images = []
-    while True:
-      success, frame = cap.read()  # read the camera frame
-      if not success:
-          # del session['temp_filename']
-          break
-      cnt+=1
-      if cnt % 3 ==0:
-          continue
-      im = cv2.resize(frame, (150, 150), interpolation=cv2.INTER_AREA)
-      origins.append(frame.copy())
-      images.append(im)
-      if len(images) > 10:
-          preds = predict_batch(images)
-          for ori, pred in zip(origins, preds):
-              font = cv2.FONT_HERSHEY_SIMPLEX
-              org = (150, 150)
-              fontScale = 1
-              color = (255, 0, 0)
-              thickness = 2
-              frame = cv2.putText(ori, pred, org, font,
-                                  fontScale, color, thickness, cv2.LINE_AA)
-              out.write(frame)
-          images = []
-          origins = []
-    cap.release()
-    out.release()
-    return filename
-
-
-
-
 
 ########################     실행     ########################
 if __name__ == '__main__':
